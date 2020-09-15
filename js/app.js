ec8971326adeb2fead65c9ea8ffa400849538807
Mohamed Nehad <mohammednehad450@gmail.com>
2020-09-16 01:08:08 +0300

    feat: Build nav

--- js/app.js
+++ js/app.js
@@ -18,6 +18,9 @@
  * 
 */
 
+let sections;
+
+const SECTIONS_OBSERVER = new MutationObserver(onSectionChange);
 
 /**
  * End Global Variables
@@ -25,7 +28,27 @@
  * 
 */
 
+function onSectionChange(){
+  sections = document.querySelectorAll('section');
+  buildNav();
+}
 
+
+function createNavLink(section) {
+
+  // Container: list item
+  const li = document.createElement("li");
+
+  // Anchor to section
+  const a = document.createElement('a');
+  a.href = `#${section.id}`;
+  a.textContent = section.dataset.nav;
+  a.classList.add('menu__link');
+
+  li.appendChild(a);
+
+  return li;
+}
 
 /**
  * End Helper Functions
@@ -35,6 +58,22 @@
 
 // build the nav
 
+function buildNav() {
+
+  // Getting nav contianer
+  const navContainer = document.querySelector('#navbar__list');
+  navContainer.style.visibility = "hidden";
+
+  // Clearing old nav items
+  navContainer.innerHTML = "";
+
+  // Creating nav items
+  for (section of sections) {
+    navContainer.insertAdjacentElement("afterbegin", createNavLink(section));
+  }
+
+  navContainer.style.visibility = "visible";
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,10 +84,22 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
 // Build menu 
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Nav initial build
+  onSectionChange();
+
+  // Observing subsequent sections changes, assuming that sections are childern of main
+  const main = document.querySelector('main');
+  const config = { attributes: false, childList: true, subtree: false };
+  SECTIONS_OBSERVER.observe(main, config);
+
+});
 
 // Scroll to section on link click
 
