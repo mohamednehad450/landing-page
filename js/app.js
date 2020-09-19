2c4d80c62ebb03bdcba477a64b3ad8048e393ab0
Mohamed Nehad <mohammednehad450@gmail.com>
2020-09-19 23:37:01 +0300

    feat: Add scroll to top button

--- js/app.js
+++ js/app.js
@@ -24,6 +24,8 @@ let lastScrollY = 0;
 
 const SECTIONS_OBSERVER = new MutationObserver(onSectionChange);
 
+let scrollToTopBtn;
+
 // Used for scrollHandler
 let ticking = false;
 
@@ -98,6 +100,10 @@ function setHeaderVisibility(state) {
 }
 
 
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -153,14 +159,24 @@ function scrollToSection(hash) {
 
 // Change page header visibility
 function headerVisibilityHandler(lastScrollY) {
-  
+
   // Scrolling up, showing header
   if (lastScrollY > window.scrollY) {
     setHeaderVisibility(true);
-  } 
+  }
   // Scrolling down, hiding header
-  else if (lastScrollY < window.scrollY){
+  else if (lastScrollY < window.scrollY) {
     setHeaderVisibility(false);
+  }
+}
+
+
+function setScrollBtnVisibility(state) {
+  const currentState = !scrollToTopBtn.classList.contains('btn__hidden');
+  if (state !== currentState) {
+    state ?
+      scrollToTopBtn.classList.remove('btn__hidden') :
+      scrollToTopBtn.classList.add('btn__hidden');
   }
 }
 
@@ -204,4 +220,20 @@ window.addEventListener('scroll', scrollHandler(setActiveSection));
 // Toggle page header
 window.addEventListener('scroll', () => {
   setTimeout(headerVisibilityHandler, 0, window.scrollY);
+});
+
+
+// ScrollToTop events
+document.addEventListener('DOMContentLoaded', () => {
+  scrollToTopBtn = document.querySelector('#scroll-to-top');
+  scrollToTopBtn.onclick = scrollToTop;
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > window.innerHeight / 2) {
+    setScrollBtnVisibility(true);
+  }
+  else {
+    setScrollBtnVisibility(false);
+  }
 });
